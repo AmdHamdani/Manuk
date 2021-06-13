a76cce8f96ff3d2f1cdb5b0c677f4e15bfc7bb0c
AmdHamdani <Hamdaniryuuken@gmail.com>
2021-06-13 11:51:54 +0700

    Set check baloon position on colision stay

--- Assets/Scripts/BirdController.cs
+++ Assets/Scripts/BirdController.cs
@@ -24,7 +24,7 @@ public class BirdController : MonoBehaviour
         }
     }
 
-    private void OnTriggerEnter2D(Collider2D collision)
+    private void OnTriggerStay2D(Collider2D collision)
     {
         if (collision.CompareTag("Baloon"))
         {
