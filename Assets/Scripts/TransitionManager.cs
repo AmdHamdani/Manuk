1d5ab6eb965283cd2bd9d76c4d55db1ea0c65e41
Syauqi Hakim <syauqihh@gmail.com>
2021-06-13 19:52:47 +0700

    Set up main menu

---
+++ Assets/Scripts/TransitionManager.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class TransitionManager : MonoBehaviour
+{
+    public GameObject startTransition;
+    public GameObject endTransition;
+    public float startTransitionTime;
+    public float endTransitionTime;
+    // Start is called before the first frame update
+    void Start() {
+        StartCoroutine(StartTransitionPlay(startTransitionTime));
+    }
+    IEnumerator StartTransitionPlay(float transitionTime){
+        startTransition.SetActive(true);
+        yield return new WaitForSeconds(transitionTime);
+        startTransition.SetActive(false);
+    }
+
+    public void LoadScene(int levelIndex){
+        StartCoroutine(EndTransitionPlay(levelIndex));
+    }
+    IEnumerator EndTransitionPlay(int levelIndex){
+        endTransition.SetActive(true);
+        yield return new WaitForSeconds(endTransitionTime);
+        SceneManager.LoadScene(levelIndex); 
+    }
+}
