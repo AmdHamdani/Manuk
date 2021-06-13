e7105df1b658152fe4dfe376c1891ac36251351f
AmdHamdani <Hamdaniryuuken@gmail.com>
2021-06-13 22:19:06 +0700

    Add obstacle mover

---
+++ Assets/Scripts/ObstacleMover.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObstacleMover : MonoBehaviour
+{
+    private float speed;
+    private ObstacleSpawner spawner;
+
+    public void SetUp(float speed, ObstacleSpawner spawner)
+    {
+        this.speed = speed;
+        this.spawner = spawner;
+    }
+
+    private void Update()
+    {
+        transform.Translate(Vector2.down * speed * Time.deltaTime);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Debug.Log("Enter");
+        if(collision.CompareTag("Remover"))
+        {
+            if(spawner != null)
+            {
+                spawner.totalObstacles--;
+            }
+            Destroy(gameObject);
+        }
+    }
+}
