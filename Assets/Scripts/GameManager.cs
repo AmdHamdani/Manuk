18e7ada69d07a22b2230b699949ec572cc63d7db
AmdHamdani <Hamdaniryuuken@gmail.com>
2021-06-13 23:37:48 +0700

    Add check game over & win in game manager

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -4,5 +4,39 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
-    
+    public float startCheckAfter = 10f;
+    public float gameLength = 5f;
+    public bool isRunning = true;
+    public GameObject winPanel;
+    public GameObject gameOverPanel;
+    public List<GameObject> birds = new List<GameObject>();
+
+    private void Start()
+    {
+        StartCoroutine(CheckGameOver());
+        StartCoroutine(CheckWin());
+    }
+
+    private IEnumerator CheckGameOver()
+    {
+        yield return new WaitForSeconds(startCheckAfter);
+        while (isRunning)
+        {
+            yield return new WaitForSeconds(.1f);
+            if (birds.Count <= 0)
+            {
+                gameOverPanel.SetActive(true);
+                isRunning = false;
+            }
+        }
+    }
+
+    private IEnumerator CheckWin()
+    {
+        yield return new WaitForSeconds(gameLength);
+        if(birds.Count > 0)
+        {
+            winPanel.SetActive(true);
+        }
+    }
 }
